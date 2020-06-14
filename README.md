7d2c5c9ed53c06919e47998e1fe17919463dce12
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2020-06-13 23:01:01 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# lifelab-web
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class App extends React.Component<any , any>{
+    render(): React.ReactNode {
+        return (<div>lifelab</div>)
+    }
+}
+
+export default App;
