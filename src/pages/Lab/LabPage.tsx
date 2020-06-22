aa4df29a9f9600d9dc23ce0b085eec4002060873
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2020-06-21 22:29:39 -0600

    Fix LabPage routing issue

--- src/pages/Lab/LabPage.tsx
+++ src/pages/Lab/LabPage.tsx
@@ -2,7 +2,7 @@
 import { css, jsx } from "@emotion/core";
 import React from "react";
 import Page from "../../components/Page";
-import { useHistory, useParams, useRouteMatch } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import Button from "../../components/Button";
 
 const navButtonsContainerStyle = css`
@@ -13,13 +13,14 @@ const navButtonsContainerStyle = css`
 
 export default function LabPage(): JSX.Element {
   const { labId } = useParams();
-  const { url } = useRouteMatch();
   const history = useHistory();
   return (
     <Page>
       <h1>Lab #{labId}</h1>
       <div css={navButtonsContainerStyle}>
-        <Button onClick={() => history.push(`${url}issues`)}>Issues</Button>
+        <Button onClick={() => history.push(`/labs/${labId}/issues/`)}>
+          Issues
+        </Button>
         <Button>Experiments (coming soon)</Button>
       </div>
     </Page>
