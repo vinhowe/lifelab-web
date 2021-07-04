fe0420186bfec3d8c8e271e24ff3bea439761425
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-07-03 19:51:58 -0600

    Add placeholder for draggable list item

--- src/components/ReorderableIssueList.tsx
+++ src/components/ReorderableIssueList.tsx
@@ -86,6 +86,7 @@ export default function ReorderableIssueList({
                   )}
                 </Draggable>
               ))}
+            {provided.placeholder}
           </div>
         )}
       </Droppable>
